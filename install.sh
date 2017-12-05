90440e8dda181011876077a9fd3be869e295f8ca
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-12-05 21:48:08 +0100

    moving nvm installation before interactive installation steps

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt update -y
 sudo apt install -y oracle-java8-installer
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
