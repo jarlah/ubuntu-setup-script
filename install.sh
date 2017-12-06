2d4e180600e3487314c9a8434dbdc6b994925890
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-12-06 12:40:24 +0100

    hipchat

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
+wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
+sudo apt update
+sudo apt -y install hipchat4 
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt update -y
 sudo apt install -y oracle-java8-installer
